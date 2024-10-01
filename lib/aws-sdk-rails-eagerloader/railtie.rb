0540db70a5eacaeddcd35c1941d89ef8cd95c732
Nick Schwaderer <nick.schwaderer@shopify.com>
2024-10-01 11:24:46 +0100

    Push to gem

--- lib/aws-sdk-rails-eagerloader/railtie.rb
+++ lib/aws-sdk-rails-eagerloader/railtie.rb
@@ -1,15 +1,17 @@
-module AwsSdkRailsEagerloader
-  class Railtie < ::Rails::Railtie
-    initializer 'aws-sdk-rails-eagerloader.sdk_eager_load' do |app|
-      app.config.before_eager_load do
-        app.config.eager_load_namespaces << Aws
-      end
+# lib/aws-sdk-rails-eagerloader/railtie.rb
 
+require 'rails/railtie'
+
+module AwsSdkRailsEagerloader
+  class Railtie < Rails::Railtie
+    initializer 'aws-sdk-rails.sdk_eager_load' do
+      config.before_eager_load do
+        config.eager_load_namespaces << Aws
+      end
       Aws.define_singleton_method(:eager_load!) do
         Aws.constants.each do |c|
           m = Aws.const_get(c)
           next unless m.is_a?(Module)
-
           m.constants.each do |constant|
             m.const_get(constant)
           end
